54b420eecea6ac9ef87d932bf0cad7b659caff92
MahirFadha <159881303+MahirFadha@users.noreply.github.com>
2024-09-03 15:44:30 +0700

    Add files via upload

---
+++ src/Hp/Handphone.java
@@ -0,0 +1,16 @@
+package Hp;
+
+public class Handphone {
+    protected void hidupkan(){
+        System.out.println("Handphone hidup ...");
+    }
+    protected void lakukanPanggilan(){
+        System.out.println("Kring, kring, kring … panggilan dilakukan");
+    }
+    protected void kirimSMS(){
+        System.out.println("Dung, dung … sms berhasil terkirim");
+    }
+    protected void matikan(){
+        System.out.println("Handphone mati …");
+    }
+}
